9a10f22620e06e67a82b801aab61f66377fc7fe7
javlon <isomurodov@gmail.com>
2017-01-23 03:28:47 +0300

    main

---
+++ src/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+
+/**
+ * Created by javlon on 21.01.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Point a = new Point(2, new double[]{-1, 0});
+        Point b = new Point(2, new double[]{1, 1});
+        Point[] points = new Point[4];
+        points[0] = new Point(2, new double[]{-1, 2});
+        points[1] = new Point(2, new double[]{0, 1});
+        points[2] = new Point(2, new double[]{0, -2});
+        points[3] = new Point(2, new double[]{1, 1});
+        Tree t1 = Build.doTree(points, 0, true);
+        Tree t2 = Build.doTree(points, 0, false);
+        int n1 = t1.query(a, b);
+        int n2 = t2.query(a, b);
+        System.out.println("Layered range tree found: " + n1 +
+                "\nSimple range tree found: " + n2);
+    }
+}
