8edaeda426c9391e7455beb42d9a562dac9cb3cb
javlon <isomurodov@gmail.com>
2017-02-06 19:38:13 +0300

    main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,8 +10,8 @@ public class Main {
         points[1] = new Point(2, new double[]{0, 1});
         points[2] = new Point(2, new double[]{0, -2});
         points[3] = new Point(2, new double[]{1, 1});
-        Tree t1 = Build.doTree(points, true);
-        Tree t2 = Build.doTree(points, false);
+        Tree t1 = Build.doTree(points,  true);
+        Tree t2 = Build.doTree(points,  false);
         int n1 = t1.query(a, b);
         int n2 = t2.query(a, b);
         System.out.println("Layered range tree found: " + n1 +
